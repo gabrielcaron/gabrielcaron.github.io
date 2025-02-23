7729e26cb2b051bab37c4fd6e1906ca4c7671e2f
Gabriel Caron <gabriel.caron@gmail.com>
2025-02-23 12:36:10 -0500

    [Accordéon] Intégration initiale

---
+++ liaisons/js/accordeon.js
@@ -0,0 +1,32 @@
+/**
+ * Accordeon Class
+ * @author Gabriel Caron <gabriel.caron@gmail.com>
+ * @copyright © 2025, Gabriel Caron. Tous droits réservés.
+ * @version 1.0
+ * @param {HTMLElement} refAccordeon - L'accordéon HTML
+ * @return {void}
+ */
+class Accordeon {
+	constructor(refAccordeon) {
+		this.monAccordeon = refAccordeon;
+		this.monBouton = this.monAccordeon.querySelector(".accordeon__bouton");
+		this.monBouton.addEventListener("click", this.toggleAccordeon.bind(this));
+		this.estOuvert = Boolean(this.monBouton.getAttribute("aria-expanded"));
+		// Par défaut, les accordéons sont ouverts. Si JavaScript est actif, les accordéons sont fermés.
+		this.toggleAccordeon();
+	}
+	toggleAccordeon() {
+		this.monBouton.setAttribute(
+			"aria-expanded",
+			`${(this.estOuvert = !this.estOuvert)}`
+		);
+		this.monAccordeon.classList.toggle("accordeon--active");
+	}
+}
+// Instanciation des accordéons (si présents)
+const Accordeons = document.querySelectorAll(".accordeon");
+Accordeons.forEach((AccordeonElement) => {
+	new Accordeon(AccordeonElement);
+	console.log("Accordéon initialisé avec succès.");
+});
+//# sourceMappingURL=accordeon.js.map
